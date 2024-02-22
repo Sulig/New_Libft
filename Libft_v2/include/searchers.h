6fba68708c1cdaa0126bf4e3e81cc597bb9abde2
Sadoming <suligvondooka@gmail.com>
2024-02-22 19:39:17 +0100

    Update searchers.h

--- Libft_v2/include/searchers.h
+++ Libft_v2/include/searchers.h
@@ -21,5 +21,6 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_search_str(char **arr, char *to_search);
 
 #endif
